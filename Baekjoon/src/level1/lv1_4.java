7fe2fcb430237baed208b07197bd304b2ecb1db0
starrysara7 <starrysara@naver.com>
2018-04-22 20:35:09 +0900

    baekjoon level_1

---
+++ Baekjoon/src/level1/lv1_4.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class lv1_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("맞은 문제: 3");
+		System.out.println("아이디: bb" );
+	}
+
+}
---
+++ Baekjoon/src/level1/lv1_7.java
@@ -0,0 +1,12 @@
+package level1;
+
+public class lv1_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Baekjoon");
+		System.out.println("Online Judge");
+	}
+
+}
